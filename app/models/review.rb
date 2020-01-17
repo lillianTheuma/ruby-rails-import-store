c9217f8811cff127c0a9b141ebf329b65600e4d7
Student <example@example.com>
2020-01-17 09:26:48 -0800

    Added models

---
+++ app/models/review.rb
@@ -0,0 +1,16 @@
+class Review < ApplicationRecord
+  belongs_to :products
+
+  validates :author, presence: true
+  validates :content_body, presence: true
+  validates :rating, presence: true
+  validates_length_of :content_body, maximum: 250
+  validates_length_of :content_body, minimum: 50
+  validates :rating, :inclusion=> { :in => [1,2,3,4,5] }
+
+  before_save(:titleize_author)
+  private
+    def titleize_author
+      self.author = self.author.titleize
+    end
+end
