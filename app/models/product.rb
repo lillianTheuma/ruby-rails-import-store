6e189c758d3c3168c787321c7a120ed1fc0092a9
Student <example@example.com>
2020-01-17 10:21:22 -0800

    Added scopes

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,17 @@ class Product < ApplicationRecord
   validates :cost, presence: true
   validates :country_of_origin, presence: true
 
+  scope :three_most_recent, -> { order(created_at: :desc).limit(3)}
+  scope :most_reviews, -> {(
+    select("product.id, product.name, product.cost, product.country_of_origin, count(reviews.id) as reviews_count")
+    .joins(:reviews)
+    .group("products.id")
+    .order("tasks_count DESC")
+    .limit(1)
+  )}
+  scope :name, -> (name_parameter) { where(name: name_parameter)}
+  scope :search, -> (name_parameter) { where("name like ?", "%#{name_parameter}%")}
+
   before_save(:titleize_product)
   private
     def titleize_product
