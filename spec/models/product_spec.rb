abc2f923875eb3db8ab1ca4f99360897c92bcbe1
Student <example@example.com>
2020-01-17 09:40:42 -0800

    Removed undesired test

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -4,7 +4,6 @@ describe Product do
   # Tests will go here.
   it { should have_many(:reviews) }
   it { should validate_presence_of :name }
-  it { should validate_length_of(:name).is_at_most(100) }
   it("titleizes the name of an product") do
     product = Product.create({name: "marketing"})
     expect(product.name()).to(eq("Marketing"))
