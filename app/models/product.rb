39c1d43d45665bf6a03819a6da8f79c1de2eef0f
Student <example@example.com>
2020-01-17 10:59:34 -0800

    Added scope

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,7 @@ class Product < ApplicationRecord
   validates :cost, presence: true
   validates :country_of_origin, presence: true
 
+  scope :american, -> { where(country_of_origin: "United States")}
   scope :three_most_recent, -> { order(created_at: :desc).limit(3)}
   scope :most_reviews, -> {(
     select("product.id, product.name, product.cost, product.country_of_origin, count(reviews.id) as reviews_count")
@@ -13,7 +14,8 @@ class Product < ApplicationRecord
     .order("tasks_count DESC")
     .limit(1)
   )}
-  scope :name, -> (name_parameter) { where(name: name_parameter)}
+
+  scope :get_by_name, -> (name_parameter) { where(name: name_parameter)}
   scope :search, -> (name_parameter) { where("name like ?", "%#{name_parameter}%")}
 
   before_save(:titleize_product)
