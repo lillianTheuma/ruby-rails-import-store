842432904bbe8087d0f3d8c7e615024ab91196d3
Student <example@example.com>
2020-01-24 12:01:03 -0800

    added vendor id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_24_173040) do
+ActiveRecord::Schema.define(version: 2020_01_24_192408) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,9 +52,16 @@ ActiveRecord::Schema.define(version: 2020_01_24_173040) do
     t.datetime "last_sign_in_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "vendor_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "vendors", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+  end
+
   add_foreign_key "reviews", "products"
+  add_foreign_key "users", "vendors"
 end
