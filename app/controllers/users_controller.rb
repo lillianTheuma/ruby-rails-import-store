4d51abb86ac5ae00a9b11e5deeae5477fe74d2f5
Student <example@example.com>
2020-01-24 17:51:44 -0800

    Hourly commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,14 +5,86 @@ class UsersController < ApplicationController
       current_user.roles.each do |role|
         @roles.push role.name
       end
+      @required_roles = ["Admin"] & @roles
     end
-    @required_roles = ["Admin"] & @roles
-    if @required_roles.any?
+    if @required_roles == nil
+      flash[:alert] = "Insufficient Priveleges."
+      redirect_to '/'
+    else
+      if @required_roles.any?
       @users = User.all
       render :index
-    else
+      else
       flash[:alert] = "Insufficient Priveleges."
-      redirect_to application_path
+      redirect_to '/'
+      end
+    end
+  end
+
+  def remove_role
+    if user_signed_in?
+      @roles = []
+      current_user.roles.each do |role|
+        @roles.push role.name
+      end
+      @required_roles = ["Admin"]
+    end
+    if @required_roles == nil
+      flash[:alert] = "Insufficient Priveleges."
+      redirect_to '/'
+    else
+      if @required_roles.any?
+        @user = User.find(params[:user_id])
+        @roles = []
+        @user.roles.each do |role|
+          @roles.push role.name
+        end
+
+        @role = Role.find(params[:role_id])
+        if @roles.include?(@role.name)
+          @user.roles.delete @role
+          redirect_to user_path(@user)
+        else
+          flash[:alert] = "This user already has that role!"
+          redirect_to user_path(@user)
+        end
+      else
+        flash[:alert] = "Insufficient Priveleges."
+        redirect_to '/'
+      end
+    end
+  end
+
+  def add_role
+    if user_signed_in?
+      @roles = []
+      current_user.roles.each do |role|
+        @roles.push role.name
+      end
+      @required_roles = ["Admin"]
+    end
+    if @required_roles == nil
+      flash[:alert] = "Insufficient Priveleges."
+      redirect_to '/'
+    else
+      if @required_roles.any?
+        @user = User.find(params[:user_id])
+        @roles = []
+        @user.roles.each do |role|
+          @roles.push role.name
+        end
+        @role = Role.find(params[:role_id])
+        if @roles.include?(@role.name)
+          flash[:alert] = "This user already has that role!"
+          redirect_to user_path(@user)
+        else
+          @user.roles << @role
+          redirect_to user_path(@user)
+        end
+      else
+        flash[:alert] = "Insufficient Priveleges."
+        redirect_to '/'
+      end
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,9 @@ Rails.application.routes.draw do
   end
 
   resources :vendors
-  resources :users
+  resources :users do
+    get '/add_role/:role_id' => 'users#add_role'
+    get '/remove_role/:role_id' => 'users#remove_role'
+  end
   resources :roles
 end
